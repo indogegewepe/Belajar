ee0b215f7a56bfc029f6dc5083234e3ebc35428f
indogegewepoe <bagasuwaidha007@gmail.com>
2025-01-24 20:15:28 +0700

    C++

---
+++ 06_printing/printing.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+int main(){
+	std::cout << "halo sodara-sodara sekalian\n";
+	std::cout << "ini adalah baris baru.";
+	std::cout << "ini bukan baris baru.";
+	std::cout << " ini akhir kalimat." << std::endl;
+	return 0;
+}
